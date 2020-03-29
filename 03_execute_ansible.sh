5e642969ddfadc53aa90316c0629f2818398bba9
o310yusuke <yusuke310@gmail.com>
2020-03-29 16:59:49 +0900

    first commit

---
+++ 03_execute_ansible.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# デフォルトモード　check=構文チェックモード
+_MODE_DEFAULT=check
+_MODE_EXECUTE=execute
+
+mode=
+
+# モードチェック
+if [ "$1" = "" ]; then
+    # 未入力、不一致のときは、チェックモードで実行
+    printf '実行モードを入力（構文チェック：check, 実行：execute）: '
+    read mode
+else
+    mode="$1"
+fi
+
+if [ "$mode" = $_MODE_EXECUTE ] || [ "$mode" = "e" ]; then
+    # ansible-playbook 01_setup.yml -i 01_inventory
+    printf '間違って実行しないようにコメントアウト中\n'
+else
+    printf '構文チェックを実行 -----\n'
+    # ansible構文チェック
+    ansible-playbook --syntax-check 01_setup.yml -i 01_inventory
+fi
