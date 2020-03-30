e0d81cfb0dd818a93ad28f5b3a127e97c2e5b7da
o310yusuke <yusuke310@gmail.com>
2020-03-30 18:02:51 +0900

    add dry run settings

--- 03_execute_ansible.sh
+++ 03_execute_ansible.sh
@@ -2,6 +2,7 @@
 
 # デフォルトモード　check=構文チェックモード
 _MODE_DEFAULT=check
+_MODE_DRY_RUN=run
 _MODE_EXECUTE=execute
 
 mode=
@@ -9,7 +10,7 @@ mode=
 # モードチェック
 if [ "$1" = "" ]; then
     # 未入力、不一致のときは、チェックモードで実行
-    printf '実行モードを入力（構文チェック：check, 実行：execute）: '
+    printf '実行モードを入力（構文チェック：check, 実行チェック；run, 実行：execute）: '
     read mode
 else
     # 実行時の引数があれば、それを利用
@@ -19,6 +20,9 @@ fi
 if [ "$mode" = $_MODE_EXECUTE ] || [ "$mode" = "e" ]; then
     # ansible-playbook 01_setup.yml -i 01_inventory
     printf '間違って実行しないようにコメントアウト中\n'
+elif [ "$mode" = $_MODE_DRY_RUN ] || [ "$mode" = "r" ]; then
+    printf '実際には変更を行わずにお試し実行（DryRun） -----\n'
+    ansible-playbook --check 01_setup.yml -i 01_inventory
 else
     printf '構文チェックを実行 -----\n'
     # ansible構文チェック
